47e0ae8c2d38070a77f0f2941b276f6f4289a793
Asimeet Padhee <asimeet@onco.com>
2020-04-15 00:07:49 +0530

    update-cors-for-ec2

--- CustomerProfileBE/server.js
+++ CustomerProfileBE/server.js
@@ -10,19 +10,21 @@ const cors = require('cors');
 mongoose.connect(config['mongo-url']);
 app.use(cors());
 var allowedOrigins = ['http://localhost:4200',
-                      'http://18.206.158.114:4200'];
+    'http://0:0:0:0:4200',
+    'http://18.206.158.114:4200'
+];
 app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
+    origin: function (origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
                 'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
     }
-    return callback(null, true);
-  }
 }));
 
 // app.use(function (req, res, next) {
